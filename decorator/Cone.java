981fd5362b52eed1308cf26369c34ead46ad81ac
Jacob-M-Webster <jakewebster5619@gmail.com>
2025-09-07 13:40:27 -0400

    slight modifications

--- decorator/Cone.java
+++ decorator/Cone.java
@@ -1,8 +1,14 @@
 import java.util.ArrayList;
+import java.lang.StringBuilder;
 public class Cone extends IceCream {
+
+    private static final String ANSI_BROWN = "\u001B[38;5;130m";
     public Cone(){
         ArrayList<String> lines = FileReader.getLines("cone.txt");
-        for (String line: lines)
+        for (String line: lines) {
+            StringBuilder string = new StringBuilder();
+            string.append(ANSI_BROWN).append(line);
             asciiArt.add(0, line);
+        }
     }
 }
